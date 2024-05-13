fd17e6e59f1acd1725932729bc76ae32c7131747
Iresh Eranga <iresheranga2020al@gmail.com>
2024-05-13 23:21:54 +0530

    Verify User Complete and merged

--- src/restaurant/management/system/Login.java
+++ src/restaurant/management/system/Login.java
@@ -247,6 +247,9 @@ public class LogIn extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(LogIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
